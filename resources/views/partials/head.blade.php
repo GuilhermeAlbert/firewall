2dd33ce09323636ebe6e0db5f64758ec1a674537
Guilherme Albert <guilhermealbert14@gmail.com>
2019-03-27 18:08:48 -0300

    Enviando modificações no template

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,20 +2,17 @@
 <html lang="pt-br">
 
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Appliance Firewall">
+    <meta name="author" content="Appliance Firewall">
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <title>Appliance Firewall</title>
+	
+	<link rel="icon" href="{{ asset('img/favicon.ico') }}" sizes="16x16 32x32" type="image/ico">
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }} " rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-  <title>SB Admin 2 - Blank</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
+    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 </head>
