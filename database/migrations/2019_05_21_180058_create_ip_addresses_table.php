376c947c8fac4e14394e4aa46396b4ba98d81e6e
Guilherme Albert <guilhermealbert14@gmail.com>
2019-06-11 21:46:59 -0300

    Criando migrations de bloqueio e liberação

---
+++ database/migrations/2019_05_21_180058_create_ip_addresses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIpAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ip_addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('description');
+            $table->enum('action', ['allow', 'deny']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ip_addresses');
+    }
+}
---
+++ database/migrations/2019_06_12_003819_alter_mac_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterMacAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mac_addresses', function (Blueprint $table) {
+            $table->unsignedInteger('status_id')->default(1)->after('description');
+            $table->foreign('status_id')->references('id')->on('status'); 
+
+            $table->unsignedInteger('group_id')->default(1)->after('status_id');
+            $table->foreign('group_id')->references('id')->on('groups'); 
+        }); 
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mac_addresses', function (Blueprint $table) {
+            $table->dropColumn("group_id");
+            $table->dropColumn("status_id");
+        });
+    }
+}
